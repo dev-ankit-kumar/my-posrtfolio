915da120e4e2a85f5638dd6b67ae61d9bc4e7a85
dev-ankit-kumar <ankitkumar3971@gmail.com>
2023-01-24 02:21:58 +0530

    2nd

--- src/components/Part2.js
+++ src/components/Part2.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Part2() {
   return (
     <div className='para2'>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quibusdam sit, aspernatur sed asperiores sint natus maxime laborum cupiditate aliquid illum deserunt adipisci, at explicabo ad. Voluptatum reiciendis id, et quibusdam velit obcaecati natus neque aliquid magnam reprehenderit? Obcaecati ducimus ab cumque. Cum molestias magnam quod, architecto dignissimos tempora quibusdam?
+     <span className="para2body"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quibusdam sit, aspernatur sed asperiores sint natus maxime laborum cupiditate aliquid illum deserunt adipisci, at explicabo ad. Voluptatum reiciendis id, et quibusdam velit obcaecati natus neque aliquid magnam reprehenderit? Obcaecati ducimus ab cumque. Cum molestias magnam quod, architecto dignissimos tempora quibusdam?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus dolor ullam reiciendis unde enim quaerat incidunt quam natus recusandae? A nesciunt modi commodi? Quisquam velit ratione, accusamus quaerat vitae hic, minima, porro facilis molestias dicta neque magnam! Explicabo temporibus, voluptas, hic ullam accusantium nulla quo, nam aperiam blanditiis tenetur iste maxime vitae optio molestiae beatae provident. Nihil vitae perspiciatis similique tempora perferendis incidunt inventore autem iure eos, nobis et ipsum, voluptates nisi voluptate iste corporis? Perspiciatis cum unde accusamus, omnis nobis at ea? Deleniti aliquam cupiditate architecto in quaerat provident ab aliquid commodi. Expedita quis aut voluptatem, itaque sequi nihil?</span>
     </div>
   )
 }
