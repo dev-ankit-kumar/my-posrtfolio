72819631fb9abb46f097d3d3f33e788d71fe65d4
dev-ankit-kumar <ankitkumar3971@gmail.com>
2023-01-26 01:38:01 +0530

    12th

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,38 @@
 import React from 'react'
-
+import { useState } from 'react';
 export default function Form() {
+
+
+
+    const [message, setMessage] = useState(true);
+    const [fname, setfname] = useState('fname');
+    const [lname, setlname] = useState('Initial value');
+    const [ph, setph] = useState('Initial value');
+
+    // 👇️ called every time input's value changes
+    const mailChange = event => {
+      setMessage(event.target.value);
+     
+    };
+    const fnameChange = event => {
+      setfname(event.target.value);
+     
+    };
+    const lnameChange = event => {
+      setlname(event.target.value);
+    };
+    const phChange = event => {
+      
+      setph(event.target.value);
+    };
+
+
+
+
+
+
   return (
-    <div>
+    <div className='contact-form-ankit'>
       
 
 
@@ -22,24 +52,24 @@ export default function Form() {
 <div className="mc-field-group">
 	<label for="mce-EMAIL">Email Address  <span className="asterisk">*</span>
 </label>
-	<input type="email" value="" name="EMAIL" className="required email" id="mce-EMAIL" required/>
+	<input type="email" value={message} name="EMAIL" className="required email" id="mce-EMAIL" onChange={mailChange}  required/>
 	<span id="mce-EMAIL-HELPERTEXT" className="helper_text"></span>
 </div>
 <div className="mc-field-group">
 	<label for="mce-FNAME">First Name  <span className="asterisk">*</span>
 </label>
-	<input type="text" value="" name="FNAME" className="required" id="mce-FNAME" required/>
+	<input type="text" value={fname} onChange={fnameChange} name="FNAME" className="required" id="mce-FNAME" required/>
 	<span id="mce-FNAME-HELPERTEXT" className="helper_text"></span>
 </div>
 <div className="mc-field-group">
 	<label for="mce-LNAME">Last Name </label>
-	<input type="text" value="" name="LNAME" className="" id="mce-LNAME"/>
+	<input type="text" value={lname} onChange={lnameChange} name="LNAME" className="" id="mce-LNAME"/>
 	<span id="mce-LNAME-HELPERTEXT" className="helper_text"></span>
 </div>
 <div className="mc-field-group size1of2">
 	<label for="mce-PHONE">Phone Number  <span className="asterisk">*</span>
 </label>
-	<input type="text" name="PHONE" className="required" value="" id="mce-PHONE" required/>
+	<input type="text" name="PHONE" className="required" value={ph} onChange={phChange} id="mce-PHONE" required/>
 	<span id="mce-PHONE-HELPERTEXT" className="helper_text"></span>
 </div>
 <div hidden="true"><input type="hidden" name="tags" value="9321239"/></div>
