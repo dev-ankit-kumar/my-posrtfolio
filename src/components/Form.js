d0073b777804c79c442280c95ea995a4c09624f7
dev-ankit-kumar <ankitkumar3971@gmail.com>
2023-01-26 01:16:28 +0530

    11th

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,50 +9,49 @@ export default function Form() {
 
 
 
-
+{/* 
 <style type="text/css">
 	#mc_embed_signup{{background:'#fff ', clear:'left', font:'14px Helvetica,Arial,sans-serif',  width:'600px'}}
-	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
+	
+</style> */}
 <div id="mc_embed_signup">
-    <form action="https://app.us11.list-manage.com/subscribe/post?u=798d89feb5f90fc583ad55501&amp;id=76b92a3be3&amp;f_id=000caae0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <form action="https://app.us11.list-manage.com/subscribe/post?u=798d89feb5f90fc583ad55501&amp;id=76b92a3be3&amp;f_id=000caae0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll">
         <h2>Subscribe</h2>
-        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-<div class="mc-field-group">
-	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+        <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
+<div className="mc-field-group">
+	<label for="mce-EMAIL">Email Address  <span className="asterisk">*</span>
 </label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required/>
-	<span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+	<input type="email" value="" name="EMAIL" className="required email" id="mce-EMAIL" required/>
+	<span id="mce-EMAIL-HELPERTEXT" className="helper_text"></span>
 </div>
-<div class="mc-field-group">
-	<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
+<div className="mc-field-group">
+	<label for="mce-FNAME">First Name  <span className="asterisk">*</span>
 </label>
-	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME" required/>
-	<span id="mce-FNAME-HELPERTEXT" class="helper_text"></span>
+	<input type="text" value="" name="FNAME" className="required" id="mce-FNAME" required/>
+	<span id="mce-FNAME-HELPERTEXT" className="helper_text"></span>
 </div>
-<div class="mc-field-group">
+<div className="mc-field-group">
 	<label for="mce-LNAME">Last Name </label>
-	<input type="text" value="" name="LNAME" class="" id="mce-LNAME"/>
-	<span id="mce-LNAME-HELPERTEXT" class="helper_text"></span>
+	<input type="text" value="" name="LNAME" className="" id="mce-LNAME"/>
+	<span id="mce-LNAME-HELPERTEXT" className="helper_text"></span>
 </div>
-<div class="mc-field-group size1of2">
-	<label for="mce-PHONE">Phone Number  <span class="asterisk">*</span>
+<div className="mc-field-group size1of2">
+	<label for="mce-PHONE">Phone Number  <span className="asterisk">*</span>
 </label>
-	<input type="text" name="PHONE" class="required" value="" id="mce-PHONE" required/>
-	<span id="mce-PHONE-HELPERTEXT" class="helper_text"></span>
+	<input type="text" name="PHONE" className="required" value="" id="mce-PHONE" required/>
+	<span id="mce-PHONE-HELPERTEXT" className="helper_text"></span>
 </div>
 <div hidden="true"><input type="hidden" name="tags" value="9321239"/></div>
-	<div id="mce-responses" class="clear foot">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
+	<div id="mce-responses" className="clear foot">
+		<div className="response" id="mce-error-response" style={{display:'none'}}></div>
+		<div className="response" id="mce-success-response" style={{display:'none'}}></div>
 	</div>    
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_798d89feb5f90fc583ad55501_76b92a3be3" tabindex="-1" value=""/></div>
-        <div class="optionalParent">
-            <div class="clear foot">
-                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/>
-                <p class="brandingLogo"><a href="http://eepurl.com/ija9WX" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"/></a></p>
+    <div style={{position: 'absolute' ,left: '-5000px', ariaHidden:'true'}} ><input type="text" name="b_798d89feb5f90fc583ad55501_76b92a3be3" tabindex="-1" value=""/></div>
+        <div className="optionalParent">
+            <div className="clear foot">
+                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/>
+                <p className="brandingLogo"><a href="http://eepurl.com/ija9WX" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"/></a></p>
             </div>
         </div>
     </div>
